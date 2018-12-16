f4b378b9f1aaf04c584e752e925b573b4b232bb5
alberic dufort <alberic.dufort@gmail.com>
2018-12-16 17:02:34 +0100

    init commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express = require('express')
+var bodyparser = require('body-parser');
+const app = express()
+const port: string = process.env.PORT || '8081'
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`server is listening on port ${port}`)
+})
