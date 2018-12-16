21406f31315b37890066ddc9c52f874480101ae3
alberic dufort <alberic.dufort@gmail.com>
2018-12-16 18:51:11 +0100

    first routes

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,32 @@ import express = require('express')
 var bodyparser = require('body-parser');
 const app = express()
 const port: string = process.env.PORT || '8081'
+import {UsersHandler} from './users'
+import {User} from './users'
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded())
 
 app.get('/', (req: any, res: any) => {
   res.write('Hello world')
+  res.end()
+})
+
+app.get('/test', (req: any, res: any) => {
+  res.write('Hello test')
+  res.end()
+})
+
+app.get('/signin', (req: any, res: any) => {
+  const user = [new User(1,"name","password")]
+  const db = new UsersHandler('./db')
+  db.save(0, user, (err: Error | null) => {
+    if (err) {
+      throw err
+    }
+    //res.json(result)
+  })
+  res.write('Hello signin')
   res.end()
 })
 
